dc62e3f7607d3c1df4a6a92ce95ee93dd19aa58d
victorsraj <90927071+victorsraj@users.noreply.github.com>
2022-08-22 12:38:17 -0700

    Add files via upload

---
+++ js/wildfirenews.js
@@ -0,0 +1,14 @@
+// const settings = {
+//   async: true,
+//   crossDomain: true,
+//   url: "https://wildlife-live1.p.rapidapi.com/news",
+//   method: "GET",
+//   headers: {
+//     "x-rapidapi-host": "wildlife-live1.p.rapidapi.com",
+//     "x-rapidapi-key": "16a069c270msh04908a472fef33fp155d06jsn2f9253760867"
+//   }
+// };
+
+// $.ajax(settings).done(function (response) {
+//   console.log(response);
+// });
